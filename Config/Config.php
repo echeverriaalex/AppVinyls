4ed5543fd6543939e24d20794b4648ad85adecaa
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-09 11:14:22 -0300

    armo la estructura del framework

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+    define("ROOT", dirname(__DIR__)."/");
+    define("FRONT_ROOT", "/AppVinilos/");
+    define("VIEWS_PATH", "Views/");
+
+    define("DB_HOST","localhost");
+    define("DB_NAME","AppVinyls");
+    define("DB_USER","root");
+    define("DB_PASS","");
+?>
