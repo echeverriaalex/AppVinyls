1d2dfbd8cacb332179c830993c3997e340157b19
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-09 11:17:16 -0300

    pdo para tarabajr con los datos de una futura base de datos

---
+++ PDO/VinylPDO.php
@@ -0,0 +1,23 @@
+<?php
+
+use Models\Vinyl;
+
+class VinylPDO{
+
+        public function Add(Vinyl $vinyl){
+
+            
+        }
+
+        public function GetAll(){
+
+        }
+
+
+        public function SearchVinylByName($diskName){
+
+
+        }
+    }
+
+?>
