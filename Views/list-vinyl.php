7fb388d54cc15162d13ac3a06cfc5173040667eb
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-10 11:29:52 -0300

    estructuro la vista de listado de vinilos

---
+++ Views/list-vinyl.php
@@ -0,0 +1,37 @@
+<div>
+    <table>
+        <thead>
+            <th> Artist </th>
+            <th> Disk name </th>
+            <th> Year edition </th>
+            <th> Country edition </th>
+            <th> Status box </th>
+            <th> Status disk </th>
+            <th> Disk format </th>
+            <th> Gender </th>
+            <th> Velocity </th>
+            <th> Observations </th>
+        </thead>
+
+        <tbody>
+            <?php 
+                if($vinylsList){
+                    foreach($vinylsList as $vinyl){ 
+            ?>
+                        <td> <?php echo $vinyl->getArtist(); ?> </td>
+                        <td> <?php echo $vinyl->getDiskName(); ?> </td>
+                        <td> <?php echo $vinyl->getYearEdition(); ?> </td>
+                        <td> <?php echo $vinyl->getCounrtyEdition(); ?> </td>
+                        <td> <?php echo $vinyl->getStatusBox(); ?> </td>
+                        <td> <?php echo $vinyl->getStatusDisk(); ?> </td>
+                        <td> <?php echo $vinyl->getDiskFormat(); ?> </td>
+                        <td> <?php echo $vinyl->getGender(); ?> </td>
+                        <td> <?php echo $vinyl->getVelocity(); ?> </td>
+                        <td> <?php echo $vinyl->getObservations(); ?> </td>
+            <?php 
+                    }
+                }
+            ?>
+        </tbody>    
+    </table>
+</div>
