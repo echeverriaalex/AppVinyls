f3121f83ac0833b6766a2bab9a593cf9ee16ead4
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-12 11:31:34 -0300

    codifico la conexion a la base de datos y los necesario para ella

--- PDO/VinylPDO.php
+++ PDO/VinylPDO.php
@@ -1,12 +1,38 @@
 <?php
+    namespace PDO;
 
-use Models\Vinyl;
+    use Exception;
+    use Models\Vinyl;
 
-class VinylPDO{
+    class VinylPDO{
+
+        private $connection;
+        private $vinylsList = array();
+        private $tableName = "Vinyl";
 
         public function Add(Vinyl $vinyl){
 
-            
+            try{
+                $query = "INSERT INTO ".$this->tableName." (artist, diskName, yearEdition, countryEdition, statusBox, statusDisk, diskFormat, gender, velocity, observations) 
+                VALUES (:artist, :diskName, :yearEdition, :countryEdition, :statusBox, :statusDisk, :diskFormat, :gender, :velocity, :observations);";
+
+                $parameters["artist"] = $vinyl->getArtist();
+                $parameters["diskName"] = $vinyl->getDiskName();
+                $parameters["yearEdition"] = $vinyl->getYearEdition();
+                $parameters["countryEdition"] = $vinyl->getCountryEdition();
+                $parameters["statusBox"] = $vinyl->getStatusBox();
+                $parameters["statusDisk"] = $vinyl->getStatusDisk();
+                $parameters["diskFormat"] = $vinyl->getDiskFormat();
+                $parameters["gender"] = $vinyl->getGender();
+                $parameters["velocity"] = $vinyl->getVelocity();
+                $parameters["observations"] = $vinyl->getObservation();
+
+                $this->connection = Connection::getInstance();
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(Exception $ex){
+                throw $ex;
+            }            
         }
 
         public function GetAll(){
