9f50ee653f644508ca4cbde2669f9f411c8f7b29
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-14 01:33:10 -0300

    busca de error objeto no localizado

--- Controllers/VinylsController.php
+++ Controllers/VinylsController.php
@@ -1,13 +1,13 @@
 <?php
-    namespace Controllers;
-    use Models\Vinyl;
+    namespace Controllers;    
     use PDO\VinylPDO;
-
+    use Models\Vinyl;
+    
     class VinylsController{
 
         private $vinylsPDO;
 
-        public function __construct(){$this->vinylsPDO = new VinylPDO;}
+        public function __construct(){$this->vinylsPDO = new VinylPDO();}
 
         public function ShowAddView(){require_once(VIEWS_PATH."add-vinyl.php");}
 
@@ -15,43 +15,44 @@
 
         public function ShowListView(){
         
-            $vinylsList = array();
+            $vinylsList = $this->vinylsPDO->GetAll();
             require_once(VIEWS_PATH."list-vinyl.php");
         }
 
         public function ShowArticlesView(){
 
-            $vinylsList = array();
+            $vinylsList = $this->vinylsPDO->GetAll();
             require_once(VIEWS_PATH."list-vinyl-articule.php");
         }
 
-
-        public function Add($artist, $diskName, $yearEdition, $countryEdition, $statusBox, $statusDisk, $diskFormat, $gender, $velocity, $observations){
+        public function Add($artist, $diskName/*, $yearEdition, $countryEdition, $statusBox, $statusDisk, $diskFormat, $gender, $velocity, $observations*/){
             
-            $exists = false; // $this->vinylsPDO->SearchVinylByName($diskName);
+            //$exists = false;  $this->vinylsPDO->SearchVinylByName($diskName);
 
-            if(!$exists){
+            //if(!$exists){
 
+                /*
                 $vinyl = new Vinyl($artist, $diskName, $yearEdition, $countryEdition, $statusBox, $statusDisk, $diskFormat, $gender, $velocity, $observations);
                 $this->vinylsPDO->Add($vinyl);
-                //$this->ShowListView();
-            }
-            else{
+                $this->ShowListView();
+                */
+            //}
+           //else{
 
-                echo"<script> alert('Error: Cannot add vinyl')</script>";
-                $this->ShowAddView();
-            }
+               // echo"<script> alert('Error: Cannot add vinyl')</script>";
+                //$this->ShowAddView();
+            //}
         }
 
         public function list(){
 
         }
 
-        public function delete(){
+        public function Delete(){
 
         }
 
-        public function edit(){
+        public function Edit(){
 
         }
     }
