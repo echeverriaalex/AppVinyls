09e2c1f67756f35e5a9ad10db6e34fc0ce21f1d0
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-14 01:31:58 -0300

    desarrollo del pdo

--- PDO/VinylPDO.php
+++ PDO/VinylPDO.php
@@ -1,13 +1,13 @@
 <?php
     namespace PDO;
 
-    use Exception;
+    use \PDOException as Exception;
+    use PDO\Connection;
     use Models\Vinyl;
 
     class VinylPDO{
 
         private $connection;
-        private $vinylsList = array();
         private $tableName = "Vinyl";
 
         public function Add(Vinyl $vinyl){
@@ -37,12 +37,55 @@
 
         public function GetAll(){
 
+            try{
+                $vinylsList = array();
+                $query = "SELECT * FROM ". $this->tableName;
+                $this->connection = Connection::getInstance();
+                $vinylsResults = $this->connection->Execute($query);
+
+                foreach($vinylsResults as $row){
+
+                    $vinyl = new Vinyl();
+                    $vinyl->setArtist($row['artist']);
+                    $vinyl->setDiskName($row['diskName']);
+                    $vinyl->setYearEdition($row['yearEdition']);
+                    $vinyl->setCountryEdition($row['countryEdition']);
+                    $vinyl->setStatusBox($row['statusBox']);
+                    $vinyl->setStatusDisk($row['statusDisk']);
+                    $vinyl->setDiskFormat($row['diskFormat']);
+                    $vinyl->setGender($row['gender']);
+                    $vinyl->setVelocity($row['velocity']);
+                    $vinyl->setObservation($row['observations']);
+
+                    var_dump($vinyl);
+
+                    array_push($vinylsList, $vinyl);
+                }
+                return $vinylsList;
+            }
+            catch(Exception $ex){
+                throw $ex;
+            }
         }
 
 
         public function SearchVinylByName($diskName){
 
 
+        }
+
+        public function Delete($vinylDiskName){
+
+            try{
+                $query = "DELETE FROM ".$this->tableName." WHERE (diskName = :vinylDiskName);";
+                $parameters['diskName'] = $vinylDiskName;
+                $this->connection = Connection::getInstance();
+                $deletedCount = $this->connection->ExecuteNonQuery($query, $parameters);
+                return $deletedCount;
+            }
+            catch(Exception $ex){
+                throw $ex;
+            }
         }
     }
 
