d6b46ac562b8da3f5d4d941cd8ec403819d48800
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-14 01:32:34 -0300

    vista para agregar vinilos

--- Views/add-vinyl.php
+++ Views/add-vinyl.php
@@ -1,63 +1,97 @@
 <div>
-    <form action="<?php echo FRONT_ROOT?>Vinyls/Add" method="POST">
-        <p>cargar discos</p><hr>
-        <label for="Artista o Banda"> Artista o Banda</label><br>
-        <input type="Artista o banda" id="Artista o Banda" name="Artista o Banda" placeholder="artista"><br>
-        <label for="Nombre de disco">Nombre de disco</label><br>
-        <input type="Nombre de disco" id=" Nombre de disco" name=" Nombre de disco"><br>
-        <!--...................año de edicion.................-->
-        <label for="año">Seleccione un año</label>
-        <input type="date" id="año" name="año"><br>
-        <!-------..............pais...........-->
-        <label for="pais">Seleccione un pais</label><br>
-        <input type="Pais" id="pais" name="pais"><br>
-        <!--.............value...............-->
-        <label for="Genero">Seleccione un genero</label>
-        <select id="Genero" name="Genero"> 
-            <option value="Rock">Rock</option>
-            <option value="Salsa">Salsa</option>
-            <option value="Pop">Pop</option>
-            <option value="Bachata">Bachata</option>
-        </select>
+    <h2> Add vinyl</h2>
 
-        <label for="Estado de caja">Estado de caja</label> 
-        <select id=" Estado de caja" name=" Estado de caja"> 
-            <option value="Dañada">Dañada</option>
-            <option value="Buen estado">Buen estado</option>
-            <option value="Nueva">Nueva</option>
-            <option value="No tiene">No tiene</option>
-        </select>
+    <form action="<?php echo FRONT_ROOT?>Vinilos/Add" method="POST">
 
-        <label for="Estado">Estado de disco</label>  
-        <select id="Estado" name="Genero"> 
-            <option value="Dañado">Dañado</option>
-            <option value="Buen estado">Buen estado</option>
-            <option value="Nuevo">Nuevo</option>
-             
-        </select><br>
+        
+        <div>           
+            <label for="artist"> Artist </label>
+            <input type="artist" id="artist" name="artist">
+        </div>
 
-         <!--...................formato   y velocidad...........-->
-        <p>Formato</p>
-        <input type="radio" id="lp" name="gender" value="lp">
-        <label for="lp">lP</label>
-        <input type="radio" id="EP" name="gender" value="EP">
-        <label for="EP">EP</label>
-        <input type="radio" id="Singles" name="gender" value="Singles">
-        <label for="Singles">Singles</label>
+        <div>
+            <label for="diskName"> Disk name </label>
+            <input type="diskName" id=" diskName" name=" diskName"> 
+        </div>
+        <!--
+        <div>
+            <label for="yearEdition"> Year edition </label>
+            <input type="date" id="yearEdition" name="yearEdition">
+        </div>
 
-        <label for="Velocidadad">Velocidad</label>  
-        <select id="Velocidad" name="Velocidad"> 
-            <option value="Lenta">Lenta</option>
-            <option value="Media">Media</option>
-            <option value="Rapida">Rapida</option>     
-        </select>
-         <!--...................observaciones.................-->
-        <p>observaciones</p>
-        <textarea name="observaciones" id="" cols="40" rows="10"></textarea><br>
-        <!--..........subir archivo...............-->
-        <h4>seleccione un archivo</h4>
-        <input type="file" name="Foto">
+        <div>
+            <label for="countryEdition"> Country edition </label>
+            <input type="countryEdition" id="countryEdition" name="countryEdition">
+        </div>
 
-        <button type="submit"> Add vinyl</button>
+        <div>
+            <label for="statusBox"> Status box </label> 
+                <select id=" statusBox" name=" statusBox"> 
+                    <option value="Dañada">Dañada</option>
+                    <option value="Buen estado">Buen estado</option>
+                    <option value="Nueva">Nueva</option>
+                    <option value="No tiene">No tiene</option>
+                </select>            
+        </div>
+
+        <div>
+            <label for="statusDisk"> Status disk </label>  
+            <select id="statusDisk" name="g e n e r o"> 
+                <option value="Dañado">Dañado</option>
+                <option value="Buen estado">Buen estado</option>
+                <option value="Nuevo">Nuevo</option>             
+            </select>
+        </div>
+
+        <h2> Format </h2>
+        <div>
+            <input type="radio" id="lp" name="diskFormat" value="lp">
+            <label for="lp">lP</label>
+        </div>
+
+        <div>
+            <input type="radio" id="EP" name="diskFormat" value="EP">
+            <label for="EP">EP</label>
+        </div>
+
+        <div>
+            <input type="radio" id="Singles" name="diskFormat" value="Singles">
+            <label for="Singles">Singles</label>
+        </div>
+
+        <div>
+            <label for="gender"> Select a gender </label>
+            <select id="gender" name="gender"> 
+                <option value="Rock">Rock</option>
+                <option value="Salsa">Salsa</option>
+                <option value="Pop">Pop</option>
+                <option value="Bachata">Bachata</option>
+            </select>
+        </div>
+
+        <div>
+            <label for="velocity">velocity</label>  
+            <select id="velocity" name="velocity"> 
+                <option value="Lenta">Lenta</option>
+                <option value="Media">Media</option>
+                <option value="Rapida">Rapida</option>     
+            </select>
+        </div>
+
+        
+        <h2> Observations </h2>
+        <div>
+            <textarea name="observations" id="" cols="40" rows="10"></textarea>
+        </div>        
+        
+        <div>
+            <h2>Select a file</h2>
+            <input type="file" name="Foto">
+        </div>
+        -->
+
+        <div>
+            <button type="submit"> Add vinyl</button>
+        </div>
     </form>
 </div>
