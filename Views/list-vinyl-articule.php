59ff9bf6fe585cec036da93782d5c7f8e9f8d300
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-14 01:30:40 -0300

    creacion de vista en forma de articulos

--- Views/list-vinyl-articule.php
+++ Views/list-vinyl-articule.php
@@ -1,17 +1,25 @@
 <main class="main">
-    <div class="articulo">                                                    
-        <article class="article">
-            <div class="imagen"><img src="assets/img/queendisco.jpg" alt="" width="100%" height="100%"></div>
-                <div class="texto">
-                    <div><h3>Queen</h3></div>
-                    <div><h5>Hot Space</h5></div>
-                    <div><p>pais: Inglaterra ver mas...</p></div><hr>
-                
-                    <div class="btn"> 
-                    <button onclick="myFunction()" class="button">Eliminar</button>
-                    <a href="modificar-discos.html"  class="button">Modificar</a>
-                </div>
-            </div>             
-        </article>
+    <div class="articulo">  
+        <?php
+            if(!empty($vinylsList)){
+                foreach($vinylsList as $vinyl){        
+        ?>       
+                    <article class="article">
+                        <div class="imagen"><img src="assets/img/queendisco.jpg" alt="" width="100%" height="100%"></div>
+                            <div class="texto">
+                                <div><h3> <?php echo $vinyl->getArtist(); ?> </h3></div>
+                                <div><h5> <?php echo $vinyl->getDiskName(); ?> </h5></div>
+                                <div><p>  <?php echo $vinyl->getCountryEdition(); ?> </p></div>
+                            
+                                <div class="btn"> 
+                                <button onclick="myFunction()" class="button">Eliminar</button>
+                                <a href="modificar-discos.html"  class="button">Modificar</a>
+                            </div>
+                        </div>             
+                    </article>        
+        <?php
+                }
+            }
+        ?>
     </div>         
 </main>    
