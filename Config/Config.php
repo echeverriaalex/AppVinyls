4e353d5434eb7ada9483cfbc7e8c4f13ea4e7076
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-15 01:32:38 -0300

    Encontre el error de objeto no localizado, era que puse mal el valor de la constante

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
     namespace Config;
 
     define("ROOT", dirname(__DIR__)."/");
-    define("FRONT_ROOT", "/AppVinilos/");
+    define("FRONT_ROOT", "/AppVinyls/");
     define("VIEWS_PATH", "Views/");
 
     define("DB_HOST","localhost");
