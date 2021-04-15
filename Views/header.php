830071b89c3398560762fab6615d3b050e66c2d6
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-15 02:00:06 -0300

    agrego css

--- Views/header.php
+++ Views/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Vinyls</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo FRONT_ROOT.VIEWS_PATH ?>css/style.css">
+        <title>App Vinyls</title>
     </head>
     <body>
-    
