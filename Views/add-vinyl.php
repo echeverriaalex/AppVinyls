69cdab81e3eedb8428694bc4ae8674952d4ec486
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-15 01:58:56 -0300

    descomento el resto de los campos

--- Views/add-vinyl.php
+++ Views/add-vinyl.php
@@ -5,7 +5,7 @@
 <div>
     <h2> Add vinyl</h2>
 
-    <form action="<?php echo FRONT_ROOT?>Vinyls/Add" method="POST">
+    <form action="<?php echo FRONT_ROOT ?>Vinyls/Add" method="POST">
 
         <div>
             <h1> Add vinyl </h1>
@@ -20,7 +20,7 @@
             <label for="diskName"> Disk name </label>
             <input type="diskName" id=" diskName" name=" diskName"> 
         </div>
-        <!--
+        
         <div>
             <label for="yearEdition"> Year edition </label>
             <input type="date" id="yearEdition" name="yearEdition">
@@ -33,7 +33,7 @@
 
         <div>
             <label for="statusBox"> Status box </label> 
-                <select id=" statusBox" name=" statusBox"> 
+                <select id="statusBox" name="statusBox"> 
                     <option value="Dañada">Dañada</option>
                     <option value="Buen estado">Buen estado</option>
                     <option value="Nueva">Nueva</option>
@@ -43,7 +43,7 @@
 
         <div>
             <label for="statusDisk"> Status disk </label>  
-            <select id="statusDisk" name="g e n e r o"> 
+            <select id="statusDisk" name="statusDisk"> 
                 <option value="Dañado">Dañado</option>
                 <option value="Buen estado">Buen estado</option>
                 <option value="Nuevo">Nuevo</option>             
@@ -88,14 +88,13 @@
         
         <h2> Observations </h2>
         <div>
-            <textarea name="observations" id="" cols="40" rows="10"></textarea>
+            <textarea name="observations" id="observations" cols="40" rows="10"></textarea>
         </div>        
         
         <div>
             <h2>Select a file</h2>
             <input type="file" name="Foto">
-        </div>
-        -->
+        </div>        
 
         <div>
             <button type="submit"> Add vinyl</button>
