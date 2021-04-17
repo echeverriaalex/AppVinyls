6dc33399556131b8f490bf9514821d94bfa3f920
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-17 03:11:29 -0300

    agrego atributo imagen

--- Data/AppVinyls.sql
+++ Data/AppVinyls.sql
@@ -11,11 +11,17 @@ create table if not exists Vinyl(
     statusDisk varchar(30), 
     diskFormat varchar(20), 
     gender varchar(30), 
-    velocity varchar(30), 
-    observations varchar(140)
+    velocity varchar(30),
+    observations varchar(140),
+    image_path varchar(250)
 );
 
+drop database AppVinyls;
+drop table Vinyl;
 select * from Vinyl;
 
-insert into Vinyl(artist, diskName, yearEdition, countryEdition, statusBox, statusDisk, diskFormat, gender, velocity, observations)  values ("queen", "bohemian rapsody", 1991, "onglaterra", "excelente", "excelente", "desconocido", "rock", "normal", "extra");
-insert into Vinyl(artist, diskName, yearEdition, countryEdition, statusBox, statusDisk, diskFormat, gender, velocity, observations)  values ("acdc", "black in black", 2005, "estados unidos", "excelente", "bueno", "desconocido", "rock", "normal", "extra");
+
+insert into Vinyl(artist, diskName, yearEdition, countryEdition, statusBox, statusDisk, diskFormat, gender, velocity, observations, image_path)
+values ("queen", "bohemian rapsody", 1991, "onglaterra", "excelente", "excelente", "desconocido", "rock", "normal", "extra", "https://www.efeeme.com/wp-content/uploads/queen-20-02-15.jpg");
+insert into Vinyl(artist, diskName, yearEdition, countryEdition, statusBox, statusDisk, diskFormat, gender, velocity,observations, image_path)
+values ("acdc", "black in black", 2005, "estados unidos", "excelente", "bueno", "desconocido", "rock", "normal", "extra", "https://studiosol-a.akamaihd.net/uploadfile/letras/albuns/1/3/b/e/21801-tb_180.jpg");
