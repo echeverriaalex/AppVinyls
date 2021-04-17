3b137fdbc0d7bdd47f7403d51090ba2a24bb92cd
echeverriaalex <aleex.naahuel@outlook.com>
2021-04-17 03:16:11 -0300

    arreglo vsual de codigo

--- Views/nav.php
+++ Views/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <span> App Vinyls</span>
     <ul>
-    <li> <a href="<?php echo FRONT_ROOT ?>Home/Index"> Inicio </a> </li>
+        <li> <a href="<?php echo FRONT_ROOT ?>Home/Index"> Inicio </a> </li>
         <li> <a href="<?php echo FRONT_ROOT ?>Vinyls/ShowAddView"> Add vinyl </a> </li>
         <li> <a href="<?php echo FRONT_ROOT ?>Vinyls/ShowListView"> List vinyl </a> </li>
         <li> <a href="<?php echo FRONT_ROOT ?>Vinyls/ShowArticlesView"> List vinyl as articule</a> </li>
